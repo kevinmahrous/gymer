c947aa9e42ef3b8df17d183385827535886c06e7
Kevin Mahrous <71846550+kevinmahrous@users.noreply.github.com>
2025-07-05 03:52:19 +0300

    clean vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
-})
+});
